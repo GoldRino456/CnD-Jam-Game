cb1d4806284b4dd2be926d3307fd290578c066ad
RonnocJ <119102690+RonnocJ@users.noreply.github.com>
2026-01-14 21:02:57 -0800

    Added jump and landing sounds

--- Assets/Scripts/Player/PlayerAudio.cs
+++ Assets/Scripts/Player/PlayerAudio.cs
@@ -1,12 +1,36 @@
 using UnityEngine;
-
+using UnityEngine.Serialization;
 public class PlayerAudio : MonoBehaviour
 {
     [SerializeField] private PlayerMovement _playerMovement;
     [SerializeField] private FMODUnity.EventReference _footstepEvent;
-
+    [FMODUnity.ParamRef]
+    [FormerlySerializedAs("parameter")]
+    public string _parameter;
+    [SerializeField] private FMODUnity.EventReference _jumpEvent;
+    void Start()
+    {
+        _playerMovement.Jumped += PlayJumpSound;
+        _playerMovement.GroundedChanged += PlayLandingSound; ;
+    }
     public void PlayFootstepSound()
     {
-        if(_playerMovement._grounded) FMODUnity.RuntimeManager.PlayOneShot(_footstepEvent, transform.position);
+        if (_playerMovement._grounded)
+        {
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName(_parameter, 0);
+            FMODUnity.RuntimeManager.PlayOneShot(_footstepEvent, transform.position);
+        }
+    }
+    private void PlayJumpSound()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(_jumpEvent, transform.position);
+    }
+    private void PlayLandingSound(bool grounded, float timeSinceChanged)
+    {
+        if (grounded)
+        {
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName(_parameter, 1);
+            FMODUnity.RuntimeManager.PlayOneShot(_footstepEvent, transform.position);
+        }
     }
 }
