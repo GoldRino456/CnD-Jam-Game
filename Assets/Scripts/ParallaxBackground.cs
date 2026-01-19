6361c6864224e3639c554bf5fe3b5e0ecf959df4
RonnocJ <119102690+RonnocJ@users.noreply.github.com>
2026-01-18 20:40:52 -0800

    Alternate parallaxing and improved tile details and lighting

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -2,12 +2,13 @@ using UnityEngine;
 
 public class ParallaxBackground : MonoBehaviour
 {
+    [SerializeField] private float parallaxFactor;
     [SerializeField] private SpriteRenderer[] _bgSections;
     [SerializeField] private Camera _mainCam;
 
     private void LateUpdate()
     {
-        transform.position = new Vector3(_mainCam.transform.position.x, _mainCam.transform.position.y, 0);
+        transform.position = new Vector3(-_mainCam.transform.position.x * parallaxFactor, -_mainCam.transform.position.y * parallaxFactor, 0);
     }
 
 }
